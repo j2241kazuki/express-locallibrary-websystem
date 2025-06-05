68eddb2ee1d064966b6542d152270d688e12bd0c
渡邊和貴 <j2241@matsue-ct.ac.jp>
2025-06-05 05:35:54 +0000

    日本語化

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -23,7 +23,7 @@ exports.index = asyncHandler(async (req, res, next) => {
   ]);
 
   res.render("index", {
-    title: "Local Library Home",
+    title: "地域図書のホーム",
     book_count: numBooks,
     book_instance_count: numBookInstances,
     book_instance_available_count: numAvailableBookInstances,
